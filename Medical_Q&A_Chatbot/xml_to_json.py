91fcf145bf79d8170d91fac869e62c3095484514
shriyansh-69 <shriyansh457tiwari@gmail.com>
2026-02-03 14:59:29 +0530

    "Setting Up The Root"

--- Medical_Q&A_Chatbot/xml_to_json.py
+++ Medical_Q&A_Chatbot/xml_to_json.py
@@ -15,7 +15,37 @@ for root_dir, _, files in os.walk(xml_root):
 
         file_path = os.path.join(root_dir,files)
 
-        
+        try :
+            tree = et.parse(file_path)
+            root = tree.getroot()
+
+            qapairs = root.find("QAPairs")
+            if qapairs is None:
+                continue
+
+            for qa in qapairs.findall("QAPair"):
+                q = qa.find("Question")
+                a = qa.find("Answer")
+
+            if q is None or a is None:
+                continue
+
+
+            question = q.text.strip()
+            answer = a.text.strip()
+
+            all_qa.append({
+                "question" : question,
+                "answer" : answer 
+            })
+
+        except Exception as e:
+            
+
+
+
+
+
 
 
         
