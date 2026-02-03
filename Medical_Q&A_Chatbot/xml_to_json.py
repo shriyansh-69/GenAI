417c926b403f46f37611b71111f0b0104bbb6102
shriyansh-69 <shriyansh457tiwari@gmail.com>
2026-02-03 12:26:14 +0530

    "looping through each folder Selecting The File And Ignoring The unnecssary Value"

--- Medical_Q&A_Chatbot/xml_to_json.py
+++ Medical_Q&A_Chatbot/xml_to_json.py
@@ -3,3 +3,15 @@ import json
 import os
 
 
+xml_root = "data/xml/MedQuAD"
+output_root = "data/medquad.json"
+
+all_qa = []
+
+for root_dir, _, files in os.walk(xml_root):
+    for file  in files:
+        if not files.endswith(".xml"):
+            continue
+
+
+        
