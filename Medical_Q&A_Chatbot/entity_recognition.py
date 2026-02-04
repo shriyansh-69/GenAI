87da2de8eee549a6f87e2de486487092778f518a
shriyansh-69 <shriyansh457tiwari@gmail.com>
2026-02-04 13:05:20 +0530

    Adding diseases mentioned in our dataset

---
+++ Medical_Q&A_Chatbot/entity_recognition.py
@@ -0,0 +1,15 @@
+# This Rule Based  File Is Created For Replying In Proper Seqeunce 
+# It Is Basically Rule-Based  Medical Entity Recongition For Our Dataset
+
+DISEASES = [
+    "leukemia",
+    "cancer",
+    "diabetes",
+    "asthma",
+    "arthritis",
+    "tuberculosis",
+    "guillain-barré syndrome",
+    "gaucher disease",
+    "krabbe disease"
+]
+
