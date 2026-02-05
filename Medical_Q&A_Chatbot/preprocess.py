1c6296c9934d51fdca41e7e992e391cb43b1f632
shriyansh-69 <shriyansh457tiwari@gmail.com>
2026-02-05 12:52:46 +0530

    "Created a Function To Preprocess The Text In Dataset"

--- Medical_Q&A_Chatbot/preprocess.py
+++ Medical_Q&A_Chatbot/preprocess.py
@@ -1,6 +1,19 @@
+# This File Is Created For Process The Word In Dataset 
+
+
 import re
 import nltk
 from nltk.stem import WordNetLemmatizer
+
+lemmatizer = WordNetLemmatizer()
+
+def preprocess(text: str) -> str:
+    text = text.lower()
+    text = re.sub(r"[^a-zA-Z\s]", "", text)
+    tokens = nltk.word_tokenize(text)
+    tokens = [lemmatizer.lemmatize(token) for token in tokens]
+    return " ".join(tokens)
+
 
 
 
