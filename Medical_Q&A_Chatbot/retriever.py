7c7317f9cf6ca1fa0244764d87158ea92f2a453f
shriyansh-69 <shriyansh457tiwari@gmail.com>
2026-02-05 13:21:18 +0530

    "Using The TfidfVectorizer"

--- Medical_Q&A_Chatbot/retriever.py
+++ Medical_Q&A_Chatbot/retriever.py
@@ -8,10 +8,26 @@ from sklearn.metrics.pairwise import cosine_similarity
 
 from preprocess import preprocess
 
-
+# Loading the Dataset 
 with open("data\medquad.json","r",encoding="utf-8") as f:
     data = json.load(f)
 
+# Preprocessing s All Question's
+questions = [preprocess(item["question"]) for item in data]
+
+# Initialize TF-IDF Vectorizer
+vectorizer = TfidfVectorizer(max_features=5000)
+questions_vectorizer = vectorizer.fit_transform(questions)
 
 
-    
+
+
+
+
+
+
+
+
+
+
+
