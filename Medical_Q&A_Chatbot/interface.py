74ab57d3214c576c0ac46e05c2818c3f38c2140b
shriyansh-69 <shriyansh457tiwari@gmail.com>
2026-02-05 15:33:48 +0530

    "Importing The Streamlit Library And Function From Different File's To Make It More Functional"

--- Medical_Q&A_Chatbot/interface.py
+++ Medical_Q&A_Chatbot/interface.py
@@ -1,3 +1,7 @@
 # interface.py
 # This File Is For Creating The Interface For Application To Make IT More Attractive And Interactive 
 
+import streamlit as st
+from entity_recognition import  extract_entities
+from retriever import retrieve_answer
+
