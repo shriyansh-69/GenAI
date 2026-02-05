c3a8152a76779e830a0a237a4405e8dd6ea17282
shriyansh-69 <shriyansh457tiwari@gmail.com>
2026-02-05 15:40:33 +0530

    "Setting Up The Page Title"

--- Medical_Q&A_Chatbot/interface.py
+++ Medical_Q&A_Chatbot/interface.py
@@ -7,3 +7,11 @@ import streamlit as st
 # Function's From Different File's 
 from entity_recognition import  extract_entities
 from retriever import retrieve_answer
+
+st.set_page_config(page_title="Medical Q&A Chatbot", layout="centered",page_icon="🩺")
+
+st.title("🧑‍⚕️ Medical Q&A Chatbot")
+st.write("Ask medical questions based on the MedQuAD Dataset.")
+
+
+
