4414a35865af979eae571d45d86581bdcca3a26f
shriyansh-69 <shriyansh457tiwari@gmail.com>
2026-02-05 16:21:37 +0530

    "Changing The Way Of Look For Interface"

--- Medical_Q&A_Chatbot/interface.py
+++ Medical_Q&A_Chatbot/interface.py
@@ -13,5 +13,37 @@ st.set_page_config(page_title="Medical Q&A Chatbot", layout="centered",page_icon
 st.title("🧑‍⚕️ Medical Q&A Chatbot")
 st.write("Ask medical questions based on the MedQuAD Dataset.")
 
+# User Input 
+query = st.text_input("Enter your medical question:")
+
+if query:
+    #  Retrieve answer from dataset
+    answer = retrieve_answer(query)
+
+    st.subheader("ANSWER")
+    st.write(answer)
+
+    # Extract Medical entities
+    entities = extract_entities(query)
+
+    st.subheader("Detected Medical Entities")
+
+    entities = extract_entities(query)
+
+    if any(entities.values()):
+        for entity_type, values in entities.items():
+            if values:
+                st.write(f"**{entity_type}:** {', '.join(values)}")
+    else:
+        st.write("No medical entities detected.")
+
+
+
+st.markdown("----")
+st.caption(
+    "⚠️ This chatbot is for educational purposes only and not a substitute for professional medical advice."
+)
+
+
 
 
