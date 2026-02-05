92021fa597e5dea6221c2aa9c065bfbf324adbc3
shriyansh-69 <shriyansh457tiwari@gmail.com>
2026-02-05 15:35:53 +0530

    "Writing a Description For Libraries.  We Are Going To Use "

--- Medical_Q&A_Chatbot/interface.py
+++ Medical_Q&A_Chatbot/interface.py
@@ -1,7 +1,9 @@
 # interface.py
 # This File Is For Creating The Interface For Application To Make IT More Attractive And Interactive 
 
+# Interface Library
 import streamlit as st
+
+# Function's From Different File's 
 from entity_recognition import  extract_entities
 from retriever import retrieve_answer
-
