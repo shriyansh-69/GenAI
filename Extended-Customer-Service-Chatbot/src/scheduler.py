1923af8b433345f662e92117f0a224a31c136df4
shriyansh-69 <shriyansh457tiwari@gmail.com>
2026-02-18 13:07:56 +0530

    "Creating a Function To Update The Function On a Regular Time Basis"

--- Extended-Customer-Service-Chatbot/src/scheduler.py
+++ Extended-Customer-Service-Chatbot/src/scheduler.py
@@ -5,6 +5,20 @@ from apscheduler.schedulers.background import BackgroundScheduler
 from lang_chain_helper import create_vector_db
 
 
+def start_scheduler():
+    scheduler = BackgroundScheduler()
+    
+    # Run Every 6 Hours (You Can Change It)
+    scheduler.add_job(create_vector_db,"interval", hours=6)
+
+
+    # It Is Like Command To Start The scheduler
+    scheduler.start()
+    print("Scheduler started. Knowledge will update automatically.")
+
+
+
+
 
 
 
