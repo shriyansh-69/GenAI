68c4afa8c2ccf7ca53c433cd5eed53e4322b0879
shriyansh-69 <shriyansh457tiwari@gmail.com>
2026-02-18 12:56:31 +0530

    "Importing All The Esstinal Libraries"

---
+++ Extended-Customer-Service-Chatbot/src/scheduler.py
@@ -0,0 +1,13 @@
+# This File Is Created For Automatically Updating The Data Of The Database 
+
+# Importing Library
+from apscheduler.schedulers.background import BackgroundScheduler
+from lang_chain_helper import create_vector_db
+
+
+
+
+
+
+
+
