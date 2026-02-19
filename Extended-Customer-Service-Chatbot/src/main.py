300a9a0a78ce62074ed872ec9a76686b9696c3db
shriyansh-69 <shriyansh457tiwari@gmail.com>
2026-02-19 12:07:54 +0530

    "Cleaning The File"

--- Extended-Customer-Service-Chatbot/src/main.py
+++ Extended-Customer-Service-Chatbot/src/main.py
@@ -2,16 +2,15 @@ import streamlit as st
 from lang_chain_helper import get_qa_chain, create_vector_db
 from scheduler import start_scheduler
 
+st.set_page_config(page_title="Customer Service Chatbot", page_icon="🤖")
+
 
 if "start_scheduler" not in st.session_state:
     start_scheduler()
     st.session_state.scheduler_started = True
 
 
-st.set_page_config(page_title="Customer Service Chatbot", page_icon="🤖")
 st.title("Customer Service Chatbot 🤖")
-
-
 
 question = st.text_input("Ask a question:")
 
