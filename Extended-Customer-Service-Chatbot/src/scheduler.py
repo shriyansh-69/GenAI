76d8a8e0135dd401ee6b6390bce8894b350a02d4
shriyansh-69 <shriyansh457tiwari@gmail.com>
2026-02-19 12:02:30 +0530

    "Updated The scheduler function"

--- Extended-Customer-Service-Chatbot/src/scheduler.py
+++ Extended-Customer-Service-Chatbot/src/scheduler.py
@@ -4,17 +4,16 @@
 from apscheduler.schedulers.background import BackgroundScheduler
 from lang_chain_helper import create_vector_db
 
+scheduler = BackgroundScheduler()
+
 
 def start_scheduler():
-    scheduler = BackgroundScheduler()
+    if not scheduler.running:
+        scheduler.add_job(create_vector_db,"interval", minutes = 30)
+        scheduler.start()
+        print("Scheduler Started. Knowledge will update automatically. ")
     
-    # Run Every 6 Hours (You Can Change It)
-    scheduler.add_job(create_vector_db,"interval", hours=6)
-
-
-    # It Is Like Command To Start The scheduler
-    scheduler.start()
-    print("Scheduler started. Knowledge will update automatically.")
+    
 
 
 
