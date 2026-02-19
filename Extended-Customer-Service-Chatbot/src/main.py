9146b9c22e858a73b2a66ca066aa749054ced353
shriyansh-69 <shriyansh457tiwari@gmail.com>
2026-02-19 11:34:59 +0530

    " Add validation to the function "

--- Extended-Customer-Service-Chatbot/src/main.py
+++ Extended-Customer-Service-Chatbot/src/main.py
@@ -3,7 +3,9 @@ from lang_chain_helper import get_qa_chain, create_vector_db
 from scheduler import start_scheduler
 
 
-
+if "start_scheduler" not in st.session_state:
+    start_scheduler()
+    st.session_state.scheduler_started = True
 
 
 st.set_page_config(page_title="Customer Service Chatbot", page_icon="🤖")
